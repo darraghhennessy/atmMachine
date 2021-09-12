04ce15ea66125b06aa925d329e38b6ebda52f709
Darragh Hennessy <darraghhennessy@gmail.com>
2021-09-12 18:04:43 +0100

    added single unit test

--- src/test/java/com/example/atm/AtmApplicationTests.java
+++ src/test/java/com/example/atm/AtmApplicationTests.java
@@ -1,13 +1,24 @@
 package com.example.atm;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class AtmApplicationTests {
 
+	@Autowired
+	private BankController bankController;
+
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	void takesCorrectPIN() throws Exception {
+		assertThat(bankController.withdrawFunds(123456789,1234,300)).contains("Incorrect PIN");
 	}
 
 }
